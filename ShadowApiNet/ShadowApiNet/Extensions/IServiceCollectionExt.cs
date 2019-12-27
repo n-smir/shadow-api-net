19baba1ee58f8103bec5f387db2c52e44f180f56
Nikita Smirnov <smirnnikita@gmail.com>
2019-12-27 22:28:33 +0100

    added initial version

---
+++ ShadowApiNet/ShadowApiNet/Extensions/IServiceCollectionExt.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShadowApiNet.Interfaces;
+
+namespace ShadowApiNet.Extensions
+{
+    public static class IServiceCollectionExt
+    {
+        private const string DefaultUriPath = "/dataapi";
+
+        public static IServiceCollection AddShadowApi(this IServiceCollection services, DbContext context, string rootUriPath = DefaultUriPath)
+        {
+            return services.AddSingleton<IApiResolver>(new ApiResolver(context, rootUriPath));
+        }
+    }
+}
---
+++ ShadowApiNet/ShadowApiNet/Interfaces/IApiResolver.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ShadowApiNet.Interfaces
+{
+    internal interface IApiResolver
+    {
+        string RootUriPath { get; }
+        DbContext Context { get; }
+        Task<HttpContext> ResolveRequest(HttpContext httpContext);
+    }
+}
