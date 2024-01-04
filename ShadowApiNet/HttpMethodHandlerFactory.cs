ddedc1e2cb6d661f9296031b6607924d60ddb63a
Nick Smirnov <smirnnikita@gmail.com>
2024-01-04 01:26:53 +0000

    Throw NotSupportedException on an unknown HTTP method

--- ShadowApiNet/HttpMethodHandlerFactory.cs
+++ ShadowApiNet/HttpMethodHandlerFactory.cs
@@ -29,7 +29,7 @@ namespace ShadowApiNet
                 case string s when s == HttpMethods.Trace:
                     return new HttpTraceHandler();
                 default:
-                    throw new NotImplementedException();
+                    throw new NotSupportedException($"Unknown HTTP method: {httpMethod}");
             }
         }
     }
